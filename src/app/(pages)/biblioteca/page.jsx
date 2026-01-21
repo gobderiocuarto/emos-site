0ee4771e614df53ad3696db0ba425823e3c1d887
Salvador Coco <salvadorcocosanchez@gmail.com>
2026-01-21 08:29:56 -0300

    Programas en biblioteca ambiental

--- src/app/(pages)/biblioteca/page.jsx
+++ src/app/(pages)/biblioteca/page.jsx
@@ -1,9 +1,17 @@
 import BannerBibliotecaList from "@/app/ui/ambiental/BannerBibliotecaList";
+import HeaderSection from "@/app/ui/layout/HeaderSection";
 import React from "react";
 
 export default function page() {
   return (
     <main className="formalities formalities-page" data-read>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <HeaderSection title="Biblioteca Ambiental" />
+          </div>
+        </div>
+      </div>
       <BannerBibliotecaList />
     </main>
   );
