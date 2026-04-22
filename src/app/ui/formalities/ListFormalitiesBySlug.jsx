807111f375d15857435ea7fec221e067483ac851
Salvador Coco <salvadorcocosanchez@gmail.com>
2026-04-22 10:00:29 -0300

    Fix header section y tramites

--- src/app/ui/formalities/ListFormalitiesBySlug.jsx
+++ src/app/ui/formalities/ListFormalitiesBySlug.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { fetchFormalities } from "@/app/lib/DataFormalities";
 import FormalitiesCard from "./CardFormality";
 import Link from "next/link";
-import HeaderSection from "../layout/HeaderSection";
 
 export default async function ListFormalitiesBySlug({ area }) {
   if (!area) return null;
@@ -14,12 +13,6 @@ export default async function ListFormalitiesBySlug({ area }) {
   return (
     <section className="formalities formalities-categories" data-read>
       <div className="container">
-        <HeaderSection
-          title="¿Que necesitás hacer?"
-          subtitle="Tramites y Servicios"
-          bgImage="/images/calidad_del_agua_potable_analisis_de_agua.webp"
-          bgPosition="center 45%"
-        />{" "}
         <h3 className="text-dark">
           Trámites <small>({allFormalities.length})</small>
         </h3>
@@ -29,10 +22,7 @@ export default async function ListFormalitiesBySlug({ area }) {
           <>
             <div className="row g-3 formalities-list mt-2">
               {formalities.map((formality) => (
-                <div
-                  className="col-12 col-md-6 col-lg-4 d-flex"
-                  key={formality.id}
-                >
+                <div className="col-12 col-md-6 d-flex" key={formality.id}>
                   <FormalitiesCard formality={formality} />
                 </div>
               ))}
--- src/app/ui/news/ListNews.jsx
+++ src/app/ui/news/ListNews.jsx
@@ -1,7 +1,6 @@
 import React, { Suspense } from "react";
 import { fetchNews } from "@/app/lib/DataNews";
 import CardNews from "./CardNews";
-import HeaderSection from "../layout/HeaderSection";
 
 export default async function ListNews({ page, limit, area }) {
   const posts = await fetchNews({ page, limit, area });
@@ -11,12 +10,6 @@ export default async function ListNews({ page, limit, area }) {
   return (
     <section data-read className="news-list">
       <div className="row ">
-        <HeaderSection
-          title="Últimas Noticias"
-          subtitle="Mantenete informado sobre la Emos"
-          bgImage="/images/parquesarmiento.webp"
-          bgPosition="center 65%"
-        />
         <Suspense fallback={<div>Cargando...</div>}>
           {posts.map((post) => (
             <CardNews key={post.id} post={post} />
